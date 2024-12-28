2f5c0f58546e4b3bb4645fa6c14985bf0a2b2849
MasihMuhammadi <masihmuhammadi202@gmail.com>
2024-12-29 00:05:08 +0430

    add new codes

--- app/auth/teacher/addStudent.tsx
+++ app/auth/teacher/addStudent.tsx
@@ -7,20 +7,21 @@ import StudentInfo from "../../components/studentInfomationForm/studentInfo";
 import Buttons from "../../components/buttons";
 import axios from "axios";
 
-
 const AddStudent = ({ data }: { data: any }) => {
-    const [step, setStep] = useState(1)
-    return <>
-
-        <div className="mt-2">
-            <div className={`flex flex-col h-[450px] justifiy-center items-center  overflow-x-hidden  transition-all duration-1000 `}>
-                <div className={`transitin-all duration-1000 mt-5`}>
-                    {<StudentInfo data={data} />}
-                </div>
-            </div>
-
+  const [step, setStep] = useState(1);
+  return (
+    <>
+      <div className="mt-2">
+        <div
+          className={`flex flex-col h-[450px] justifiy-center items-center  overflow-x-hidden  transition-all duration-1000 `}
+        >
+          <div className={`transitin-all duration-1000 mt-5`}>
+            {<StudentInfo data={data} />}
+          </div>
         </div>
-    </>;
+      </div>
+    </>
+  );
 };
 
 export default AddStudent;
--- app/components/table/editOrDelete.tsx
+++ app/components/table/editOrDelete.tsx
@@ -1,23 +1,22 @@
-
-import React from 'react';
+import React from "react";
 
 const EditOrDelete = () => {
-    return (
-        <>
-            <div className='bg-white shadow-md z-[100] rounded-xl absolute px-5 top-10 left-5'>
-                <div className='flex flex-col gap-y-2'>
-                    <div className='flex flex-row gap-x-2'>
-                        <p>||</p>
-                        <p>Edit</p>
-                    </div>
-                    <div className='flex flex-row gap-x-2'>
-                        <p>||</p>
-                        <p>Delete</p>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="bg-white shadow-md z-[100] rounded-xl absolute px-5 top-10 left-5">
+        <div className="flex flex-col gap-y-2">
+          <div className="flex flex-row gap-x-2">
+            <p>||</p>
+            <p>Edit</p>
+          </div>
+          <div className="flex flex-row gap-x-2">
+            <p>||</p>
+            <p>Delete</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default EditOrDelete;
